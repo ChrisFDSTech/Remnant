e5e2a0645c3ff0d4961b249a721c758ceaa63b61
ChrisFDSTech <146366279+ChrisFDSTech@users.noreply.github.com>
2024-07-09 13:35:45 -0400

    Restructure

--- BostonPost/AppInstall_BostonPost.ps1
+++ BostonPost/AppInstall_BostonPost.ps1
@@ -11,13 +11,51 @@ $Arguments = @(
 # Define the directory
 $directoryName = "FDS\Temp"
 $directoryPath = Join-Path -Path $env:ProgramData -ChildPath $directoryName
+$filesPath = Join-Path -Path $directoryPath -ChildPath "Files"
+$zipPath = Join-Path -Path $filesPath -ChildPath "Boston Post Installer.zip.001"
 
 # Define the GitHub URLs for the files
+$bpFiles = "https://github.com/ChrisFDSTech/Remnant/tree/main/BostonPost/Files"
+
+# Start logging if any part of the process fails
+$logPath = "C:\FDSLogs"
+if (-not (Test-Path $logPath)) {
+    try {
+        New-Item -ItemType Directory -Path $logPath -Force | Out-Null
+    }
+    catch {
+        Write-Host "Failed to create directory: $logPath"
+        Start-Transcript -Path (Join-Path -Path $env:ProgramData -ChildPath "$($PackageName)_$(Get-Date -Format 'MM-dd-yyyy_HH:mm:ss').txt") -Append
+    }
+}
+else {
+    Start-Transcript -Path (Join-Path -Path $logPath -ChildPath "$($PackageName)_$(Get-Date -Format 'MM-dd-yyyy_HH:mm:ss').txt") -Append
+}
+
+# Create the directory
+if (-not (Test-Path $directoryPath)) {
+    try {
+        Write-Host "Creating directory: $directoryPath"
+        New-Item -ItemType Directory -Path $directoryPath -Force | Out-Null
+    }
+    catch {
+        Write-Warning "Failed to create directory: $directoryPath"
+    }
+}
 
 
+# Download Zip file from GitHub to the FDS Directory
+Write-Host "Downloading icon file from: $bpFiles"
+try {
+    Invoke-WebRequest -Uri $bpFiles -OutFile (Join-Path -Path $directoryPath -ChildPath "Files")
+}
+catch {
+    Write-Warning "Failed to download icon file from: $bpFiles"
+}
 
-Invoke-WebRequest -Uri "https://update.bostonpost.com/pm/9.2.49/pm_9.2.49_full.exe" -OutFile "C:\Users\Public\Downloads\pm_9.2.49_full.exe"
-Invoke-WebRequest -Uri "https://update.bostonpost.com/pm/9.2.49/pm_9.2.49_full.msi" -OutFile "C:\Users\Public\Downloads\pm_9.2.49_full.msi"
+# Exract contents of zip file
+Expand-Archive -Path $zipPath -DestinationPath $filesPath -Force
 
-Start-Process -FilePath "C:\Users\Public\Downloads\\pm_9.2.49_full.exe" -ArgumentList $Arguments -Wait
-Start-Process -Filepath "C:\Users\Public\Downloads\pm_9.2.49_full.msi" /passive
+#Installation
+Start-Process -FilePath "$filesPath\pm_9.2.49_full.exe" -ArgumentList $Arguments -Wait
+Start-Process -Filepath "$filesPath\pm_9.2.49_full.msi" /passive
